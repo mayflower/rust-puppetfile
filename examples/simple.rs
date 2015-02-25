70017fc74174f0ac0d58b82c40b160f2b60377b5
Robin Gloster <robin@loc-com.de>
2015-02-25 16:05:30 +0100

    cleanup

--- examples/simple.rs
+++ examples/simple.rs
@@ -1,17 +1,21 @@
+#![feature(env, fs, io, path)]
+
 extern crate puppetfile;
 
-use std::old_io::File;
-use std::os;
+use std::env;
+use std::fs::File;
+use std::io::Read;
+use std::path::Path;
 use std::str;
 use puppetfile::Puppetfile;
 
 fn main() {
-    let args = os::args();
-    let file_raw_bytes = File::open(&Path::new(args[1].as_slice())).read_to_end().unwrap();
-    let puppetfile_contents = str::from_utf8(file_raw_bytes.as_slice()).unwrap();
-    let puppetfile = Puppetfile::parse(puppetfile_contents).unwrap_or(
+    let args = env::args().collect::<Vec<_>>();
+    let mut puppetfile_contents = String::new();
+    File::open(&Path::new(&args[1])).unwrap().read_to_string(&mut puppetfile_contents);
+    let puppetfile = Puppetfile::parse(&puppetfile_contents).unwrap_or(
         Puppetfile {
-            forge: String::from_str("https://forge.puppetlabs.com"),
+            forge: "https://forge.puppetlabs.com".to_string(),
             modules: vec![]
         }
     );
